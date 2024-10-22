28fddf00b16e32f7067c4a18fa161abd073eac7e
VishnevetskayaSasha <visneveckaya.a@gmail.com>
2024-10-22 16:09:50 +0300

    создание калькулятора калорий

--- js/script.js
+++ js/script.js
@@ -559,4 +559,77 @@ window.addEventListener("DOMContentLoaded", ()=>{
 
         });
     });
+
+    // калькулятор калорий
+    
+    const result = document.querySelector(".calculating__result span");
+    let sex = "female", height, weight, age, ratio = 1.375; // указываем значения изначальноактиных блоков
+
+    function calcTotal() {
+        // если не выбрано хоть одно из полей, код дальше не пойдет
+        if(!sex || !height || !weight || !age || !ratio){
+            result.textContent = "______";
+            return;
+        }
+        // формула из инет для женщин и мудчин
+        if(sex ==="female") {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+    }
+
+    calcTotal();
+
+    // получаем данные статичных блоков 
+    function getStaticInformation(parentSelector,  activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        elements.forEach(elem => {
+            elem.addEventListener("click", (e) => {
+                if(e.target.getAttribute("data-ratio")) {
+                    ratio = +e.target.getAttribute("data-ratio");
+                } else {
+                    sex = e.target.getAttribute("id");
+                }
+    
+    
+                elements.forEach(elem => {
+                    elem.classList.remove(activeClass);
+                })
+    
+                e.target.classList.add(activeClass);
+                calcTotal();
+            })
+        })
+    }
+
+    // так как у нас 2 родительских элемента, вызываем функцию для каждого 
+    getStaticInformation("#gender", "calculating__choose-item_active");
+    getStaticInformation(".calculating__choose_big", "calculating__choose-item_active");
+
+    // получаем данные динамических элементов 
+    function getDinamicInformation(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener("input", () => {
+            switch(input.getAttribute("id")) {
+                case "height": 
+                    height =+ input.value;
+                    break;
+                case "weight": 
+                    weight =+ input.value;
+                    break;
+                case "age": 
+                    age =+ input.value;
+                    break;
+            } 
+            calcTotal();
+        }); 
+    }
+
+    // вызываем ыункцию для каждого из селекторов
+    getDinamicInformation("#height");
+    getDinamicInformation("#weight");
+    getDinamicInformation("#age");
 });
